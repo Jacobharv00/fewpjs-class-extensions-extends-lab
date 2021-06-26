a804db971adb36e4bf3233fdf2a736c5f48d5f2e
jacob <jacobharv00@gmail.com>
2021-06-26 14:04:56 -0600

    still working on

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides
+    this.count = this.sides.length
+  }
+
+  get countSides() {
+    return this.sides.length
+  }
+
+
+
+
+
+}
